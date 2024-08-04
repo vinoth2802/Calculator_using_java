2330e6f6ba8cdc396cce1b55a9f6dc7e3e65f908
vinoth2802 <vinothkumarramyajeeva@gmail.com>
2024-08-04 20:52:13 +0530

    finalized

--- src/main/java/com/calculator/App.java
+++ src/main/java/com/calculator/App.java
@@ -3,7 +3,8 @@ package main.java.com.calculator;
 import main.java.com.calculator.ui.CalculatorUI;
 
 public class App {
-        public static void main(String[] args) {
-            new CalculatorUI();
-        }
+
+    public static void main(String[] args) {
+        new CalculatorUI();
+    }
 }
